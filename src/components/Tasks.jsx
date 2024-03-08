1370043587a6758c66fff85b3dc09176067bd61f
husseinhareb <shtam.hareb12@gmail.com>
2024-03-08 12:20:12 +0200

    new task

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,28 +1,38 @@
 import React, { useState } from "react";
 import "../styles/Tasks.css";
-
+import barsIco from "../assets/icons/barsIco.svg";
 function Tasks() {
-    const [showInput,setShowInput] =useState(false);
+    const [showInput, setShowInput] = useState(false);
 
-    const addTask = () =>{
-        if(!showInput){
+    const addTask = () => {
+        if (!showInput) {
             setShowInput(true);
         }
     }
+    const handleCancel = () => {
+        setShowInput(false)
+    }
+
     return (
         <div className="tasks-container">
             <div className="tasks-div">
                 <div className="top-task">
                     <p className="task-title">Tasks</p>
-                    <button className="tasks-settings">:</button>
+                    <button className="tasks-settings">
+                       S <img src={barsIco} alt="Icon" />
+                    </button>
                 </div>
+                <hr></hr>
                 <div className="add-task">
-                    {showInput &&(
+                    {showInput && (
                         <div className="new-task">
-                        <input type="radio"/>
-                        <input type="text"/>
-                        <button>Cancel</button>
-                        <button>Add</button>
+                            <input type="text"
+                                placeholder="Enter your task" 
+                                className="task-input"/>
+                            <div className="new-task-bottom">
+                            <button onClick={handleCancel}>Cancel</button>
+                            <button>Add</button>
+                            </div>
                         </div>
                     )}
                     <button className="add-task-btn" onClick={addTask}>Add Task</button>
@@ -30,7 +40,7 @@ function Tasks() {
             </div>
         </div>
     );
-    
+
 }
 
 export default Tasks;
