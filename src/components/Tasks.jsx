4cd74ed6d292f2d32c62df3a46b878e132040e85
husseinhareb <shtam.hareb12@gmail.com>
2024-03-08 14:09:29 +0200

    new task

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,17 +1,47 @@
 import React, { useState } from "react";
 import "../styles/Tasks.css";
 import barsIco from "../assets/icons/barsIco.svg";
+import { invoke } from '@tauri-apps/api/tauri';
+
 function Tasks() {
     const [showInput, setShowInput] = useState(false);
+    const [taskContent, setTaskContent] = useState("");
+    const [taskTime, setTaskTime] = useState(25);
+    const [tasks, setTasks] = useState([]); 
 
     const addTask = () => {
         if (!showInput) {
             setShowInput(true);
         }
-    }
+    };
+
     const handleCancel = () => {
-        setShowInput(false)
-    }
+        setShowInput(false);
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const newTask = { id: tasks.length + 1, task: taskContent, expectedTime: taskTime };
+        setTasks([...tasks, newTask]);
+        setShowInput(false);
+        setTaskContent("");
+        setTaskTime(25);
+  
+        try {
+            await invoke("set_task", {newTask});
+            console.log(newTask);
+          } catch (error) {
+            console.error('Error while sending data to backend:', error);
+          }
+    };
+
+    const handleTaskContent = (e) => {
+        setTaskContent(e.target.value);
+    };
+
+    const handleTaskTime = (e) => {
+        setTaskTime(e.target.value);
+    };
 
     return (
         <div className="tasks-container">
@@ -19,28 +49,41 @@ function Tasks() {
                 <div className="top-task">
                     <p className="task-title">Tasks</p>
                     <button className="tasks-settings">
-                       S <img src={barsIco} alt="Icon" />
+                        S <img src={barsIco} alt="Icon" />
                     </button>
                 </div>
                 <hr></hr>
                 <div className="add-task">
                     {showInput && (
-                        <div className="new-task">
-                            <input type="text"
-                                placeholder="Enter your task" 
-                                className="task-input"/>
-                            <div className="new-task-bottom">
-                            <button onClick={handleCancel}>Cancel</button>
-                            <button>Add</button>
+                        <form onSubmit={handleSubmit}>
+                            <div className="new-task">
+                                <input
+                                    type="text"
+                                    onChange={handleTaskContent}
+                                    value={taskContent} 
+                                    placeholder="Enter your task"
+                                    className="task-input"
+                                />
+                                <input
+                                    type="number"
+                                    onChange={handleTaskTime}
+                                    value={taskTime} 
+                                    className="task-time"
+                                />
+                                <div className="new-task-bottom">
+                                    <button onClick={handleCancel} className="cancel-button">
+                                        Cancel
+                                    </button>
+                                    <button type="submit" className="add-button">Add</button>
+                                </div>
                             </div>
-                        </div>
+                        </form>
                     )}
                     <button className="add-task-btn" onClick={addTask}>Add Task</button>
                 </div>
             </div>
         </div>
     );
-
 }
 
 export default Tasks;
