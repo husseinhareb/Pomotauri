a40c91b5d5c1b0d037cc42f4a44ed45e34d86353
husseinhareb <shtam.hareb12@gmail.com>
2024-03-15 23:35:08 +0200

    window's window

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,6 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use serde::{Deserialize, Serialize};
 use std::fs;
 use tauri::{AppHandle};
